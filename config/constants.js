ea4bce25c5a178492e62976ef14ef2ef62a9d487
xP-xD <tlsqlfn842@naver.com>
2024-11-07 21:42:58 +0900

    feat: constants.js 메시지 계층 표시 및 customer.html 연결

---
+++ config/constants.js
@@ -0,0 +1,35 @@
+export const MESSAGES = {
+  HELLO: "님 안녕하세요.",
+
+  BUTTON: {
+    TASK:{
+      CUSTOMER: {
+        PRODUCT: {
+          LIST: '상품 리스트',
+          INSURANCE_LIST: '보험 상품 리스트',
+          LOAN_LIST: '대출 상품 리스트'
+        },
+        MANAGEMENT_CONTRACT: '기가입 보험 관리',
+        VIEW_ACCIDENT: '사고 신고',
+        VIEW_COMPLAINT: '민원 신청'
+      }
+
+      EMPLOYEE: {
+
+      }
+
+    }
+  }
+
+  POP_UP: {
+
+  }
+
+  COMBOBOX: {
+
+  }
+}
+
+export const COLUMN_NAME = {
+
+}
