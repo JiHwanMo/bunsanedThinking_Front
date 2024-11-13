584b9754d30a1bcb88f16822b88ab364d15c37fe
0711kc <0711kc@naver.com>
2024-11-14 00:45:25 +0900

    refactor: loanManagement FetchHandler 적용

--- js/utils/apiUtils/apiHandler/employee/loanManagement/loanManagement.js
+++ js/utils/apiUtils/apiHandler/employee/loanManagement/loanManagement.js
@@ -1,75 +1,4 @@
-const fetchGetHandler = async (url) => {
-  try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    }
-
-    const data = await response.json(); // JSON 데이터를 파싱
-    return data; // 받아온 JSON 데이터 반환
-  } catch (error) {
-    console.error("Error fetching:", error);
-    return null; // 에러 발생 시 null 반환
-  }
-};
-
-export const fetchPostHandler = async (url, body) => {
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(body)
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    }
-  } catch (error) {
-    console.error("Error fetching:", error);
-  }
-}
-
-export const fetchPatchHandler = async (url) => {
-  try {
-    const response = await fetch(url, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    }
-  } catch (error) {
-    console.error("Error fetching:", error);
-  }
-}
-
-export const fetchDeleteHandler = async (url) => {
-  try {
-    const response = await fetch(url, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    }
-  } catch (error) {
-    console.error("Error fetching:", error);
-  }
-}
+import { fetchGetHandler, fetchPostHandler, fetchPatchHandler, fetchDeleteHandler } from "../../FetchHandler.js"
 
 export const fetchGetAllHandler = async () => {
   return await fetchGetHandler("http://localhost:8080/employee/loanManagement/getAll"); // URL을 전달
@@ -92,7 +21,7 @@ export const fetchAddCollateralProductHandler = async () => {
       monthlyIncome: 300
     }
   );
-}
+};
 
 export const fetchAddLoanProductHandler = async () => {
   return await fetchPostHandler("http://localhost:8080/employee/loanManagement/addLoanProduct",
@@ -106,16 +35,16 @@ export const fetchAddLoanProductHandler = async () => {
       monthlyIncome: 300
     }
   );
-}
+};
 
 export const fetchRequestLoanHandler = async () => {
   await fetchPostHandler("http://localhost:8080/employee/loanManagement/requestLoan?contractId=1004&money=1000&paymentType=1&result=true",)
-}
+};
 
 export const fetchUpdateLoanProductHandler = async () => {
-  await fetchPatchHandler("http://localhost:8080/employee/loanManagement/updateLoanProduct?index=1&input=Test Loan&loanId=7002005");
-}
+  await fetchPatchHandler("http://localhost:8080/employee/loanManagement/updateLoanProduct?index=1&input=Test Loan&loanId=7002004");
+};
 
 export const fetchDeleteLoanProductHandler = async () => {
   await fetchDeleteHandler("http://localhost:8080/employee/loanManagement/deleteLoanProduct?id=7002004");
-}
+};
