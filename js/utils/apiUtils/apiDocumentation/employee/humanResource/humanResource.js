a699a1632f67a1dec3e7524342b30dd54a947b87
0711kc <0711kc@naver.com>
2024-11-14 22:49:00 +0900

    refactor: HumanResource apiDocumentation 파라미터 적용

--- js/utils/apiUtils/apiDocumentation/employee/humanResource/humanResource.js
+++ js/utils/apiUtils/apiDocumentation/employee/humanResource/humanResource.js
@@ -16,19 +16,20 @@ export const fetchGetAllDepartment = async () => {
   return await fetchGetAllDepartmentHandler();
 };
 
-export const fetchGetDepartment = async () => {
-  return await fetchGetDepartmentHandler(9101);
+export const fetchGetDepartment = async (departmentId) => {
+  return await fetchGetDepartmentHandler(departmentId); // 9101
 };
 
-export const fetchGetEmployee = async () => {
-  return await fetchGetEmployeeHandler(6001);
+export const fetchGetEmployee = async (employeeId) => {
+  return await fetchGetEmployeeHandler(employeeId); // 6001
 };
 
-export const fetchAddEmployee = async () => {
-  await fetchAddEmployeeHandler(
+export const fetchAddEmployee = async (addEmployeeDTO) => {
+  await fetchAddEmployeeHandler(addEmployeeDTO);
+  /*
     {
       name: "John Doe",
-      employeePosition: 2,
+        employeePosition: 2,
       address: "123 Main St",
       phoneNumber: "123-456-7890",
       bankName: "국민은행",
@@ -38,27 +39,28 @@ export const fetchAddEmployee = async () => {
       salary: 50000,
       employmentDate: "2023-01-15",
       tempFamilyList: [
-        {
-          birthDate: "1980-05-15",
-          name: "Jane Doe",
-          relationship: 1,
-          survival: true
-        },
-        {
-          birthDate: "2010-09-22",
-          name: "Jake Doe",
-          relationship: 2,
-          survival: true
-        }
-      ]
+      {
+        birthDate: "1980-05-15",
+        name: "Jane Doe",
+        relationship: 1,
+        survival: true
+      },
+      {
+        birthDate: "2010-09-22",
+        name: "Jake Doe",
+        relationship: 2,
+        survival: true
+      }
+    ]
     }
-  );
+  */
 };
 
-export const fetchUpdateEmployee = async () => {
-  await fetchUpdateEmployeeHandler(6001, 1, "Test");
+export const fetchUpdateEmployee = async (employeeId, index, input) => {
+  // 6001, 1, "Test"
+  await fetchUpdateEmployeeHandler(employeeId, index, input);
 };
 
-export const fetchDeleteEmployee = async () => {
-  await fetchDeleteEmployeeHandler(6001);
+export const fetchDeleteEmployee = async (employeeId) => {
+  await fetchDeleteEmployeeHandler(employeeId); // 6001
 };
