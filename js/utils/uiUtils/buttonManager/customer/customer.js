5e0fb500b9f5f275a7ad51cf474c847b14a41ca5
MinJae713 <dbalsend@naver.com>
2024-11-14 16:16:25 +0900

    Customer 예시 코드(실제로도 맞음) 추가
    Customer 예시 코드(실제로도 맞음) 추가

--- js/utils/uiUtils/buttonManager/customer/customer.js
+++ js/utils/uiUtils/buttonManager/customer/customer.js
@@ -3,8 +3,49 @@
 import { BUTTON } from '../../../../../config/constants.js';
 
 export const renderButtons = () => {
+  initialButtons(BUTTON.TASK.CUSTOMER, customerTaskMapper);
+};
+
+const showProductListMenu = () => {
   const buttonContainer = document.getElementById("buttonContainer");
-  const buttonMessages = BUTTON.TASK.CUSTOMER;
+  while (buttonContainer.firstChild) buttonContainer.firstChild.remove();
+  initialButtons(BUTTON.TASK.PRODUCT_LIST, productTaskMapper);
+}
+
+const managementContract = () => {
+  alert("기가입 보헙 목록스");
+}
+
+const viewAccident = () => {
+  alert("사고 신고 목록스");
+}
+
+const viewComplaint = () => {
+  alert("민원 목록스");
+}
+
+const customerTaskMapper = {
+  PRODUCT_LIST: showProductListMenu,
+  MANAGEMENT_CONTRACT: managementContract,
+  VIEW_ACCIDENT: viewAccident,
+  VIEW_COMPLAINT: viewComplaint
+}
+
+const insuranceList = () => {
+  alert("보헙 상품 목록스")
+}
+
+const loanList = () => {
+  alert("대출 상품 목록스")
+}
+
+const productTaskMapper = {
+  INSURANCE_LIST: insuranceList,
+  LOAN_LIST: loanList
+}
+
+export const initialButtons = (buttonMessages, buttonActionMapper) => {
+  const buttonContainer = document.getElementById("buttonContainer");
 
   // 객체의 각 항목을 순회하여 버튼 생성
   Object.entries(buttonMessages).forEach(([key, name]) => {
@@ -13,10 +54,9 @@ export const renderButtons = () => {
     button.textContent = name; // 버튼에 표시할 텍스트 설정
 
     button.addEventListener("click", () => {
-      alert(`${name}`);
+      // alert(`${name}`);
+      buttonActionMapper[key]();
     });
-
     buttonContainer.appendChild(button); // 버튼을 buttonContainer에 추가
   });
-};
-
+}
