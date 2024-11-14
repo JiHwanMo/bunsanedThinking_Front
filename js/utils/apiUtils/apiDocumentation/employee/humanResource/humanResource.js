c0a181f84859a335f62d49fba8d2d42244b7cd88
0711kc <0711kc@naver.com>
2024-11-14 22:10:36 +0900

    refactor: HumanResource 파라미터 추가, Default URL 분리

--- js/utils/apiUtils/apiDocumentation/employee/humanResource/humanResource.js
+++ js/utils/apiUtils/apiDocumentation/employee/humanResource/humanResource.js
@@ -17,21 +17,48 @@ export const fetchGetAllDepartment = async () => {
 };
 
 export const fetchGetDepartment = async () => {
-  return await fetchGetDepartmentHandler();
+  return await fetchGetDepartmentHandler(9101);
 };
 
 export const fetchGetEmployee = async () => {
-  return await fetchGetEmployeeHandler();
+  return await fetchGetEmployeeHandler(6001);
 };
 
 export const fetchAddEmployee = async () => {
-  await fetchAddEmployeeHandler();
+  await fetchAddEmployeeHandler(
+    {
+      name: "John Doe",
+      employeePosition: 2,
+      address: "123 Main St",
+      phoneNumber: "123-456-7890",
+      bankName: "국민은행",
+      bankAccount: "123456789012",
+      residentRegistrationNumber: "123456-7890123",
+      departmentId: 9101,
+      salary: 50000,
+      employmentDate: "2023-01-15",
+      tempFamilyList: [
+        {
+          birthDate: "1980-05-15",
+          name: "Jane Doe",
+          relationship: 1,
+          survival: true
+        },
+        {
+          birthDate: "2010-09-22",
+          name: "Jake Doe",
+          relationship: 2,
+          survival: true
+        }
+      ]
+    }
+  );
 };
 
 export const fetchUpdateEmployee = async () => {
-  await fetchUpdateEmployeeHandler();
+  await fetchUpdateEmployeeHandler(6001, 1, "Test");
 };
 
 export const fetchDeleteEmployee = async () => {
-  await fetchDeleteEmployeeHandler();
+  await fetchDeleteEmployeeHandler(6001);
 };
--- js/utils/apiUtils/apiHandler/employee/humanResource/humanResource.js
+++ js/utils/apiUtils/apiHandler/employee/humanResource/humanResource.js
@@ -1,56 +1,31 @@
 import { fetchGetHandler, fetchPostHandler, fetchPatchHandler, fetchDeleteHandler } from "../../FetchHandler.js"
 
+const defaultURL = "http://localhost:8080/employee/humanResource";
+
 export const fetchGetAllEmployeeHandler = async () => {
-  return await fetchGetHandler("http://localhost:8080/employee/humanResource/getAllEmployee");
+  return await fetchGetHandler(`${defaultURL}/getAllEmployee`);
 };
 
 export const fetchGetAllDepartmentHandler = async () => {
-  return await fetchGetHandler("http://localhost:8080/employee/humanResource/getAllDepartment");
+  return await fetchGetHandler(`${defaultURL}/getAllDepartment`);
 };
 
-export const fetchGetDepartmentHandler = async () => {
-  return await fetchGetHandler("http://localhost:8080/employee/humanResource/getDepartment?departmentId=9101");
+export const fetchGetDepartmentHandler = async (departmentId) => {
+  return await fetchGetHandler(`${defaultURL}/getDepartment?departmentId=${departmentId}`);
 };
 
-export const fetchGetEmployeeHandler = async () => {
-  return await fetchGetHandler("http://localhost:8080/employee/humanResource/getEmployee?employeeId=6001101");
+export const fetchGetEmployeeHandler = async (employeeId) => {
+  return await fetchGetHandler(`${defaultURL}/getEmployee?employeeId=${employeeId}`);
 };
 
-export const fetchAddEmployeeHandler = async () => {
-  await fetchPostHandler("http://localhost:8080/employee/humanResource/addEmployee",
-    {
-      name: "John Doe",
-      employeePosition: 2,
-      address: "123 Main St",
-      phoneNumber: "123-456-7890",
-      bankName: "국민은행",
-      bankAccount: "123456789012",
-      residentRegistrationNumber: "123456-7890123",
-      departmentId: 9101,
-      salary: 50000,
-      employmentDate: "2023-01-15",
-      tempFamilyList: [
-        {
-          birthDate: "1980-05-15",
-          name: "Jane Doe",
-          relationship: 1,
-          survival: true
-        },
-        {
-          birthDate: "2010-09-22",
-          name: "Jake Doe",
-          relationship: 2,
-          survival: true
-        }
-      ]
-    }
-  );
+export const fetchAddEmployeeHandler = async (addEmployeeDTO) => {
+  await fetchPostHandler(`${defaultURL}/addEmployee`, addEmployeeDTO);
 };
 
-export const fetchUpdateEmployeeHandler = async () => {
-  await fetchPatchHandler("http://localhost:8080/employee/humanResource/updateEmployee?employeeId=60018022&index=1&input=Test");
+export const fetchUpdateEmployeeHandler = async (employeeId, index, input) => {
+  await fetchPatchHandler(`${defaultURL}/updateEmployee?employeeId=${employeeId}&index=${index}&input=${input}`);
 };
 
-export const fetchDeleteEmployeeHandler = async () => {
-  await fetchDeleteHandler("http://localhost:8080/employee/humanResource/deleteEmployee?employeeId=60018022");
+export const fetchDeleteEmployeeHandler = async (employeeId) => {
+  await fetchDeleteHandler(`${defaultURL}/deleteEmployee?employeeId=${employeeId}`);
 };
