a6c1fc0bcc0e8869da1224ac840865b6acde68be
xP-xD <tlsqlfn842@naver.com>
2024-11-14 23:45:29 +0900

    DOCS: constants.js 구조 예시 작성

--- config/constants.js
+++ config/constants.js
@@ -19,9 +19,20 @@ export const BUTTON = {
         TERMINATION: '해지 신청',
       },
     },
-    // EMPLOYEE: {
-    //
-    // }
+
+    EMPLOYEE: {
+      UNDERWRITING: {
+        HOME: {
+          REVIEW_ACQUISITION: '인수 심사',
+          APPLY_COPERATION: '공동 인수 신청',
+          APPLY_REINSURANCE: '재보험 신청'
+        },
+        APPLY_REINSURANCE:{
+          APPLY_INSURANCE: "보험 신청",
+          CANCLE: "취소"
+        }
+      }
+    }
   }
 };
 
