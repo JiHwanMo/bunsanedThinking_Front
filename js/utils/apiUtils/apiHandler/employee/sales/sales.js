1ac1cfcf74138dd16c5e728923d1e25e7c2f907e
xP-xD <tlsqlfn842@naver.com>
2024-11-14 16:11:04 +0900

    feat: underWriting.js, sales.js, productManagement.js 헨들러랑 도큐먼트 연동

---
+++ js/utils/apiUtils/apiHandler/employee/sales/sales.js
@@ -0,0 +1,63 @@
+import { fetchGetHandler, fetchPostHandler, fetchPatchHandler, fetchDeleteHandler } from '../../FetchHandler.js';
+
+const defaultUrl = "http://localhost:8080/sales";
+
+export const fetchEvaluateSalesPerformanceHandler = async (evaluate, id) => {
+}
+
+export const fetchHandleInsuranceConsultationHandler = async (id) => {
+}
+
+export const fetchInduceInsuranceProductHandler = async (induceInsuranceProductDTO) => {
+}
+
+export const fetchInduceLoanProductHandler = async (induceInsuranceProductDTO) => {
+}
+
+export const fetchGetAllEmployeeHandler = async () => {
+}
+
+export const fetchGetEmployeeHandler = async (id) => {
+}
+
+export const fetchGetSalesHandler = async (id) => {
+}
+
+export const fetchGetAllCounselHandler = async () => {
+}
+
+export const fetchGetCounselHandler = async (id) => {
+}
+
+export const fetchGetAllProductHandler = async () => {
+}
+
+export const fetchAddDiseaseHistoryHandler = async (induceDiseaseHistoryDTO) => {
+}
+
+export const fetchUpdateContractCountHandler = async (id, contractCount) => {
+}
+
+export const fetchGetAllDiseaseInsuranceHandler = async () => {
+}
+
+export const fetchGetAllInjuryInsuranceHandler = async () => {
+}
+
+export const fetchGetAllAutomobileInsuranceHandler = async () => {
+}
+
+export const fetchGetAllCollateralLoanHandler = async () => {
+}
+
+export const fetchGetAllFixedDepositLoanHandler = async () => {
+}
+
+export const fetchGetAllInsuranceContractLoanHandler = async () => {
+}
+
+export const fetchGetSalesContractCountHandler = async (id) => {
+}
+
+export const fetchSetContractCountHandler = async (contractCount, id) => {
+}
