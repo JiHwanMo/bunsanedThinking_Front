ebe2fd4916b60782fee3962422e4420bf290f1b2
0711kc <0711kc@naver.com>
2024-11-21 23:35:28 +0900

    refactor: FinancialAccountant tableRenderer의 detail.js에서 버튼 부분 buttonManager로 분리

--- js/utils/uiUtils/tableRenderer/employee/financialAccountant/detail.js
+++ js/utils/uiUtils/tableRenderer/employee/financialAccountant/detail.js
@@ -18,8 +18,7 @@ const paymentDetail = (data) => {
 const context = {
   HANDLE_PAYMENT_DETAIL: {
     detailGetter: paymentDetail,
-    fetchGetById: fetchGetPaymentDetail,
-    buttons: BUTTON.TASK.EMPLOYEE.FINANCIAL_ACCOUNTANT.HANDLE_PAYMENT_DETAIL
+    fetchGetById: fetchGetPaymentDetail
   }
 }
 
@@ -32,7 +31,6 @@ export const renderDetails = async () => {
   if (selectedDataId) {
     const selectedData = await context[type].fetchGetById(selectedDataId);
     renderDetailsTable(selectedData);
-    renderButtons();
   }
 };
 
@@ -82,34 +80,3 @@ const renderDetailsTable = (data) => {
     detailsTable.querySelector("tbody").appendChild(row);
   });
 };
-
-const renderButtons = () => {
-  initialButtons(context[sessionStorage.getItem("currentType")].buttons, financialAccountantTaskMapper);
-};
-
-const initialButtons = (buttonMessages, buttonActionMapper) => {
-  const buttonContainer = document.getElementById("buttonContainer");
-  // 객체의 각 항목을 순회하여 버튼 생성
-  Object.entries(buttonMessages).forEach(([key, name]) => {
-    const button = document.createElement("div");
-    button.className = "button-item";
-    button.textContent = name; // 버튼에 표시할 텍스트 설정
-
-    button.addEventListener("click", buttonActionMapper[key]);
-
-    buttonContainer.appendChild(button); // 버튼을 buttonContainer에 추가
-  });
-}
-
-const handlePaymentDetail = () => {
-  alert("지급 - 재무회계");
-}
-
-const cancel = () => {
-  alert("취소 - 재무회계");
-}
-
-const financialAccountantTaskMapper = {
-  PAY: handlePaymentDetail,
-  CANCEL: cancel
-}
