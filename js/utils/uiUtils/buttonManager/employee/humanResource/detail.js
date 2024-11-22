35b58806093f650dec816b9b7a63910322fedad1
0711kc <0711kc@naver.com>
2024-11-22 19:53:09 +0900

    refactor: 사용하지 않는 코드 제거

--- js/utils/uiUtils/buttonManager/employee/humanResource/detail.js
+++ js/utils/uiUtils/buttonManager/employee/humanResource/detail.js
@@ -1,12 +1,6 @@
 import {BUTTON} from "../../../../../../config/employee/humanResource/humanResource.js";
 import {initialButtons} from "../../../common/buttonUtils.js";
 
-const context = {
-  MANAGEMENT_EMPLOYEE: {
-    buttons: BUTTON.TASK.EMPLOYEE.HUMAN_RESOURCE.MANAGEMENT_EMPLOYEE
-  }
-}
-
 export const renderButtons = () => {
   const type = sessionStorage.getItem("currentType");
   initialButtons(BUTTON.TASK.EMPLOYEE.HUMAN_RESOURCE[type], humanResourceTaskMapper[type]);
