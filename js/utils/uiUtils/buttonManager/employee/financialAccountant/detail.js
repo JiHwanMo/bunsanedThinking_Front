652d6a90213a382abbebd5fba959758d0e8dec8f
0711kc <0711kc@naver.com>
2024-11-24 18:37:27 +0900

    feat: FinancialAccountant 나머지 버튼 구현

--- js/utils/uiUtils/buttonManager/employee/financialAccountant/detail.js
+++ js/utils/uiUtils/buttonManager/employee/financialAccountant/detail.js
@@ -1,17 +1,27 @@
 import {BUTTON} from "../../../../../../config/employee/financialAccountant/financialAccountant.js";
 import {initialButtons} from "../../../common/buttonUtils.js";
+import {
+  fetchHandlePayment
+} from "../../../../apiUtils/apiDocumentation/employee/financialAccountant/financialAccountant.js";
 
 export const renderButtons = () => {
   const type = sessionStorage.getItem("currentType");
   initialButtons(BUTTON.TASK.EMPLOYEE.FINANCIAL_ACCOUNTANT[type], financialAccountantTaskMapper[type]);
 };
 
-const handlePaymentDetail = () => {
-  alert("지급 - 재무회계");
+const handlePaymentDetail = async () => {
+  const selectedDataId = sessionStorage.getItem("selectedDataId");
+  const employeeId = sessionStorage.getItem("id");
+
+  alert("지급 처리 하시겠습니까?");
+
+  await fetchHandlePayment(selectedDataId, employeeId);
+
+  window.location.href = "home.html";
 }
 
 const cancel = () => {
-  alert("취소 - 재무회계");
+  window.history.back();
 }
 
 const financialAccountantTaskMapper = {
