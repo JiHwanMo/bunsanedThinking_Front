27a1fd6df41833bc8822d860492da35ee7f32781
JiHwanMo <154873168+JiHwanMo@users.noreply.github.com>
2024-12-01 19:52:02 +0900

    FEMALE, MALE을 여자, 남자로 한글 변환 - 모
    FEMALE, MALE을 여자, 남자로 한글 변환 - 모

--- js/utils/uiUtils/tableRenderer/employee/customerInformationManagement/detail.js
+++ js/utils/uiUtils/tableRenderer/employee/customerInformationManagement/detail.js
@@ -15,7 +15,7 @@ const customerInformationDetail = (data) => {
     { label: DETAIL_COLUMN_NAME.CUSTOMER_LIST.PHONE_NUMBER, value: data.phoneNumber },
     { label: DETAIL_COLUMN_NAME.CUSTOMER_LIST.JOB, value: data.job },
     { label: DETAIL_COLUMN_NAME.CUSTOMER_LIST.AGE, value: data.age },
-    { label: DETAIL_COLUMN_NAME.CUSTOMER_LIST.GENDER, value: data.gender},
+    { label: DETAIL_COLUMN_NAME.CUSTOMER_LIST.GENDER, value: convertGenderToKorean(data.gender)},
     { label: DETAIL_COLUMN_NAME.CUSTOMER_LIST.RESIDENT_REGISTRATION_NUMBER, value: data.residentRegistrationNumber },
     { label: DETAIL_COLUMN_NAME.CUSTOMER_LIST.ADDRESS, value: data.address },
     { label: DETAIL_COLUMN_NAME.CUSTOMER_LIST.PROPERTY, value: data.property },
@@ -184,6 +184,12 @@ const customerInformationTaskMapper = {
     DELETE: deleteItem
   }
 }
+
+const convertGenderToKorean = (gender) => {
+  if (gender === "Male") return "남자";
+  if (gender === "Female") return "여자";
+  return gender; // 알 수 없는 값은 그대로 반환
+};
 
 
 
